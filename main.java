e332faeaafa63f696fe7f681172c7163204c70b9
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-08-15 21:56:05 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package javaapplication1;
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        contabanco p1 = new contabanco();
+        p1.setNumConta(1111);
+        p1.setDono("eu");
+        p1.abrirConta("CC");
+        p1.estado();
+        
+    }
+    
+}
